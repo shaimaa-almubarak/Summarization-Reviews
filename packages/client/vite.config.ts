a30c6799be45a6a28647487e23ee99e15b57c2be
shaimaa babiker <shaimaa.babeker95@gmail.com>
2025-12-06 20:43:29 +0800

    Fix

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
    },
    server: {
       proxy: {
-         '/api': 'http://localhost:3000',
+         '/api': 'https://server-production-2858.up.railway.app/',
       },
    },
 });
